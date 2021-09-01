9e3787145c927f35d93ed8d00b5ddfec4f56d786
enmanuel <enmanuellassis@gmail.com>
2021-09-01 17:23:50 -0400

    Add all remainded operations test

--- math/src/tests/operate.test.js
+++ math/src/tests/operate.test.js
@@ -1,7 +1,28 @@
 import operate from "../logic/operate"
-
-test('getting values from operate',()=>{
-  const result = operate('5','3','+');
-
-  expect((result)).toBe('8');
+describe('Operate testing',()=>{
+  test('Add',()=>{
+    const result = operate('5','3','+');
+  
+    expect((result)).toBe('8');
+  })
+  test('Substract',()=>{
+    const result = operate('5','3','-');
+  
+    expect((result)).toBe('2');
+  })
+  test('Multiply',()=>{
+    const result = operate('5','3','x');
+  
+    expect((result)).toBe('15');
+  })
+  test('Division',()=>{
+    const result = operate('5','2','/');
+  
+    expect((result)).toBe('2.5');
+  })
+  test('Modulus',()=>{
+    const result = operate('10','5','%');
+  
+    expect((result)).toBe('2');
+  })
 })
