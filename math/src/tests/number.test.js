f98c73e574e3fcafd1a512a416ca1c7c6a4ff2b8
Abel Lavieri <abel.lavieri@gmail.com>
2021-09-01 17:57:53 -0400

    Add render test for number component

---
+++ math/src/tests/number.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Number from '../components/number'
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Number />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
