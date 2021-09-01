082eaeb5f9489ec3ac225d70b7d3ef55a64212d9
Abel Lavieri <abel.lavieri@gmail.com>
2021-09-01 18:31:03 -0400

    Add navbar render test

---
+++ math/src/tests/Navbar.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
+import renderer from 'react-test-renderer'
+import Navbar from '../components/Navbar'
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
